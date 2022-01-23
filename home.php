bc49bc613fdf7ac24cb101be5bbc03ca0723c96d
Jhon <dijhonsdeveloper1023@gmail.com>
2022-01-23 01:37:31 -0500

    HeaderSectionOk

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!--Link Swiper-->
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <!--Link Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!--Css Link-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <!--Header Section Starts-->
+    <section class="header">
+        <a href="home.php" class="logo">Travel</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
+        </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+
+    </section>
+    <!--Header Section Ends-->
+
+
+
+
+<!--Link Swiper-->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<!--Link js-->
+<script src="js/script.js"></script>
+
+</body>
+</html>
